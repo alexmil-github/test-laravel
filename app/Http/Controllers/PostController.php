013be4505a2d9824dca5fe06bb2726edf0200bc9
User <admin@kfmgu.ru>
2023-02-07 20:57:33 +0300

    CRUD c постами

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Author;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    //Вывод всех постов
+    public function index()
+    {
+//        $posts = Post::all();
+        return Post::all();
+    }
+
+//    public function show($id)
+//    {
+//        $post = Post::find($id);
+//        return $post;
+//    }
+
+//Вывод одного поста
+    public function show(Post $post)
+    {
+        return $post;
+    }
+
+    //Создание поста
+    public function store(Request $request)
+    {
+
+        return Post::create($request->all());
+    }
+
+    //Редактирование поста
+    public function update(Post $post, Request $request)
+    {
+        $post->update($request->all());
+        $post->save();
+
+        return $post;
+    }
+
+    //Удаление поста
+
+    public function destroy($id)
+    {
+
+        $post = Post::find($id);
+
+        $post->delete();
+
+        return [
+          "deleted" => true
+        ];
+    }
+
+    //Вывод всех постов определенного автора
+
+    public function posts(Author $author)
+    {
+        return $author->posts;
+    }
+
+}
